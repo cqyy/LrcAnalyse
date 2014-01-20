e1cde5104c75693e416de8161a4ddf6eb9cd909e
kali <cqyuanye@gmail.com>
2014-01-20 15:47:08 +0800

    v0.2

---
+++ src/cn/kali/lrc/Test.java
@@ -0,0 +1,11 @@
+package cn.kali.lrc;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yy
+ * Date: 14-1-20
+ * Time: 下午3:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
---
+++ src/cn/kali/lrc/wordcount/IKAWordCounter.java
@@ -0,0 +1,11 @@
+package cn.kali.lrc.wordcount;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yy
+ * Date: 14-1-20
+ * Time: 下午2:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class IKAWordCounter {
+}
